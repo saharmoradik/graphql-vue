fed93baf67a0605e9524c4e06a8531bd9968e2f3
saharmoradik <saharmoradik@gmail.com>
2022-10-18 15:04:43 +0200

    apollo setting

--- src/main.js
+++ src/main.js
@@ -1,4 +1,29 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+//import { DefaultApolloClient } from "@vue/apollo-composable";
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+const defaultClient = new ApolloClient({
+  uri: "https://rickandmortyapi.com/graphql/",
+  cache: new InMemoryCache(),
+});
+
+const query = gql`
+  query {
+    characters {
+      results {
+        id
+        name
+        species
+      }
+    }
+  }
+`;
+
+defaultClient
+  .query({
+    query,
+  })
+  .then((res) => console.log(res));
+
+createApp(App).mount("#app");
